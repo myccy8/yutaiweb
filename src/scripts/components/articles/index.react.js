1b690e89192c6e47391f68ef9a685de22849d13d
lin-home <529184921@qq.com>
2015-05-26 22:06:10 +0800

    ok

--- src/scripts/components/articles/index.react.js
+++ src/scripts/components/articles/index.react.js
@@ -4,14 +4,31 @@
 'use strict';
 var React=require('react');
 var Router = require('react-router');
+var InfiniteScroll = require('react-infinite-scroll')(React);
 var {  Link } = Router;
 var Footer=React.createClass({
-
+    renderArticles(v,i){
+             return <div className="block">
+                 <div className="block-l"><a href={v.url}><img src={v.image} className="img" /></a></div>
+                 <div className="block-r">
+                     <dl>
+                         <dt><a style={{color: '#000000;'}} href={v.url}>{v.title}</a></dt>
+                         <dd>{v.detail}</dd>
+                     </dl>
+                 </div>
+             </div>;
+    },
     render(){
-        return(<footer>
-            <p><Link to="/">返回首页</Link></p>
-            <p>Copyright &copy; 2014 xiaozimeishi.com</p>
-        </footer>);
+        var flag=this.props.pageIndex<this.props.totalPage;
+        return( <div id="react-paginate">
+            <InfiniteScroll
+                pageStart={0}
+                loadMore={this.props.loadMore}
+                hasMore={flag}
+                loader='<div>loading...</div>'>
+                      {this.props.data.map(this.renderArticles)}
+            </InfiniteScroll>
+        </div>);
     }
 });
 module.exports=Footer;
