47616a125d5d5d5f45a032d1f3d48406fcd7b8a7
lin-home <529184921@qq.com>
2015-05-26 23:01:06 +0800

    ok

--- src/scripts/components/articles/index.react.js
+++ src/scripts/components/articles/index.react.js
@@ -14,16 +14,17 @@ var Footer=React.createClass({
     getInitialState(){
         return {
             data: [],
-            totalPage: 0
+            totalPage: 0,
+            articlesTop:{}
         };
     },
     componentWillMount(){
-        Store.bind('addProducts', this.changeState);
+        Store.bind('getarticles', this.changeState);
         style.use();
     },
     componentWillUnmount(){
         pageIndex=0;
-        Store.off('addProducts', this.changeState);
+        Store.off('getarticles', this.changeState);
         style.unuse();
     },
     componentDidMount() {
@@ -35,17 +36,22 @@ var Footer=React.createClass({
         Action.getArtcles(categoryId,id, index);
     },
     changeState() {
-        var id = this.context.router.getCurrentParams().id || 'default';
-        var products = Store.getProducts(id, pageIndex);
-        var top=Store.getProductTop();
-        this.refs.productTop.getDOMNode().innerHTML=top||"";
+        var id = this.context.router.getCurrentParams().id;
+        var articles = Store.getArticles(id, pageIndex);
         this.setState({
-            data: this.state.data.concat(products.list),
-            totalPage: products.totalPage
+            data: this.state.data.concat(articles.list),
+            totalPage: articles.totalPage,
+            articlesTop:articles.articlesTop
         });
     },
+    loadMore() {
+        setTimeout(function () {
+            pageIndex++;
+            this.changeRoute(pageIndex);
+        }.bind(this), 100);
+    },
     renderArticles(v,i){
-             return <div className="block">
+             return <div className="block" key={i}>
                  <div className="block-l"><a href={v.url}><img src={v.image} className="img" /></a></div>
                  <div className="block-r">
                      <dl>
@@ -56,15 +62,30 @@ var Footer=React.createClass({
              </div>;
     },
     render(){
-        var flag=this.props.pageIndex<this.props.totalPage;
+        var flag=pageIndex<this.state.totalPage;
         return( <div id="react-paginate">
+            <header>
+                <div className="headerimg">
+                    <img src={this.state.articlesTop.categoryImage} />
+                    <p>{this.state.articlesTop.categoryName}</p>
+                </div>
+            </header>
+            <section>
+                <p><a ><img src={this.state.articlesTop.indexImage}  className="img" /></a></p>
+                <article>
+                <dl>
+                    <dt><a style={{color: '#000000',fontSize:'1.25em'}}>{this.state.articlesTop.title}</a></dt>
+                    <dd>{this.state.articlesTop.content}</dd>
+                </dl>
+            </article>
             <InfiniteScroll
                 pageStart={0}
-                loadMore={this.props.loadMore}
+                loadMore={this.loadMore}
                 hasMore={flag}
                 loader='<div>loading...</div>'>
                       {this.state.data.map(this.renderArticles)}
             </InfiniteScroll>
+                </section>
         </div>);
     }
 });
